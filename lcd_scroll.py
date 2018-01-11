9c4e8d95c3a6b267ae21e4718fda39653d187f2e
quarterturn <github@matrixwide.com>
2018-01-11 12:35:07 -0500

    fixed bug in rss update wait condition checking

---
+++ lcd_scroll.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+# Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
+import time
+
+import Adafruit_CharLCD as LCD
+
+
+# Raspberry Pi pin configuration:
+lcd_rs        = 27  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en        = 22
+lcd_d4        = 25
+lcd_d5        = 24
+lcd_d6        = 23
+lcd_d7        = 18
+lcd_backlight = 26 
+
+# BeagleBone Black configuration:
+# lcd_rs        = 'P8_8'
+# lcd_en        = 'P8_10'
+# lcd_d4        = 'P8_18'
+# lcd_d5        = 'P8_16'
+# lcd_d6        = 'P8_14'
+# lcd_d7        = 'P8_12'
+# lcd_backlight = 'P8_7'
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 20
+lcd_rows    = 2
+
+MAX_FPS = 4
+
+
+class scroller(object):
+
+    left_spaces = 20
+    left_start = 0
+    right_end = 1
+    right_spaces = 1
+    line = 1
+    spaces_from_left = 20
+
+    def __init__(self, lcd_columns = 20, line = 1):
+        self.lcd_columns = lcd_columns
+        self.line = line
+        
+    def step(self, scroll_string):
+        self.scroll_string = scroll_string
+
+        # string equal to or larger than the display
+        if (len(self.scroll_string) >= self.lcd_columns):
+            # first just blank the line
+            if (self.left_spaces == self.lcd_columns):
+                lcd.set_cursor(0, self.line)
+                lcd.message(' ' * self.left_spaces)
+                self.left_spaces -= 1
+    	        return 1
+            # scroll on from the right
+            # until the left side of the display
+            elif (self.left_spaces > 0):
+    	        lcd.set_cursor(0, self.line)
+                lcd.message(' ' * self.left_spaces +  self.scroll_string[self.left_start:self.right_end])
+    	        self.right_end += 1
+                self.left_spaces -= 1
+    	        return 1
+            # scroll until the end of the string is on
+            # the right side of the display
+            elif (self.right_end < len(self.scroll_string)):
+                lcd.set_cursor(0, self.line) 
+    	        lcd.message(self.scroll_string[self.left_start:self.right_end])
+    	        self.left_start += 1
+    	        self.right_end += 1
+    	        return 1
+            # scroll until the end of the string is on
+            # the left side of the display
+            elif (self.right_spaces <= self.lcd_columns):
+                lcd.set_cursor(0, self.line)
+                lcd.message(self.scroll_string[self.left_start:self.right_end] + ' ' * self.right_spaces)
+                self.left_start += 1
+                self.right_spaces += 1 
+            # blank the line and signal that scrolling is done
+            else:
+                lcd.set_cursor(0, self.line)
+                lcd.message(' ' * self.lcd_columns)
+                self.left_spaces = self.lcd_columns
+                self.left_start = 0
+                self.right_end = 1
+                self.right_spaces = 1
+                self.spaces_from_left = self.lcd_columns 
+    	        return 0
+        else:
+            # first just blank the line
+            if (self.left_spaces == self.lcd_columns): 
+                lcd.set_cursor(0, self.line)
+                lcd.message(' ' * self.left_spaces)
+                self.left_spaces -= 1
+                self.spaces_from_left -= 1
+                return 1
+            # scroll on from the right
+            # until the end of the string is at the right end of the display
+            elif (self.spaces_from_left >= (self.lcd_columns - (len(self.scroll_string)))):
+                lcd.set_cursor(0, self.line)
+                lcd.message(' ' * self.spaces_from_left + self.scroll_string[0:self.right_end])
+                self.right_end += 1
+                self.spaces_from_left -= 1
+                return 1
+            # scroll until the start of the string is on
+            # the left side of the display
+            elif (self.spaces_from_left > 0):
+                lcd.set_cursor(0, self.line)
+                lcd.message(' ' * self.spaces_from_left + self.scroll_string + ' ' * self.right_spaces)
+                self.spaces_from_left -= 1
+                self.right_spaces += 1
+            # scroll until the end of the string is on
+            # the left side of the display
+            elif (self.right_spaces < self.lcd_columns):
+                lcd.set_cursor(0, self.line)
+                lcd.message(self.scroll_string[self.left_start:self.right_end] + ' ' * self.right_spaces)
+                self.left_start += 1
+                self.right_spaces += 1 
+            # blank the line and signal that scrolling is done 
+            else:   
+                lcd.set_cursor(0, self.line)
+                lcd.message(' ' * self.lcd_columns) 
+                self.left_spaces = self.lcd_columns 
+                self.left_start = 0
+                self.right_end = 1
+                self.right_spaces = 1
+                self.spaces_from_left = self.lcd_columns 
+                return 0 
+    
+    def check(self):
+        lcd.clear()
+        lcd.set_cursor(0, 0)
+        lcd.message("LINE ONE")
+        lcd.set_cursor(0, 1)
+        lcd.message("LINE TWO")
+
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+lcd.clear()
+lcd.vfd_dim(3)
+
+message = "This is too big to fit on the display"
+s = scroller(20, 1) 
+
+
+s.check()
+time.sleep(1)
+
+lastTime = 0 
+
+while True:
+     if s.step(message) == 0:
+         break
+
+     while True:
+         t = time.time()
+	 if (t - lastTime) > (1.0 / MAX_FPS): break
+ 
+     lastTime = t
+
+lcd.clear()
+
+message = "Here is another that is too big to fit"
+while True:
+     if s.step(message) == 0:
+         break
+
+     while True:
+         t = time.time()
+	 if (t - lastTime) > (1.0 / MAX_FPS): break
+ 
+     lastTime = t
